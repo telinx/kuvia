05bf88cafb73f9480e817c2525ba38a6a01fdb67
yucang.liu <yucang.liu@aidumei.com>
2019-11-22 18:00:11 +0800

    create

---
+++ lib/ui/pages/page_index.dart
@@ -0,0 +1,12 @@
+export 'detail/news_detail_page.dart';
+export 'detail/video_detail_page.dart';
+
+export 'follow/follow_page.dart';
+
+export 'user/user_page.dart';
+export 'user/user_setting_page.dart';
+export 'user/user_viewed_page.dart';
+export 'user/user_follow_page.dart';
+
+export 'video/video_page.dart';
+export 'home/home_page.dart';
